a3c8cfed1f43330392c14a64b4438fa15346bc01
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-12-01 21:21:21 +0100

    Cumunicación entre Componentes

---
+++ 12_Comunicación-entre-Componentes.jsx
@@ -0,0 +1,18 @@
+<>
+    <h1>Comunicación entre Componentes</h1>
+    <p>Tenemos 3 casos de Comunicación entre Eventos de React</p>
+    <ol>
+        <li>Comuninicación entre Componentes Padres y hijos</li>
+        <li>Comunicación entre Componentes hijos y Padres</li>
+        <li>Comunicación entre Componentes no relacionados</li>
+    </ol>
+
+    <h2>Comunicación entre un Componentes Padres a uno hijo</h2>
+
+    <p>Este es el caso mas natural del mundo de React que se hace atraves de props de un Componente Padre a uno hijo</p>
+
+    <h2>Comunicación de un Componente hijo a uno Padre</h2>
+
+    <p> Cuando tenemos la necssidade de passar datos de un Componente Hijo a un Padre lo podemos hacer atraves de los Eventosm, Simplismente passamos el Eventos atraves de props del Componente padre al Componente hijo, para que este Ejecute la funcion</p>
+
+</>
