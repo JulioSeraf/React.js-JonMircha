e4ee02ffba8a618beddd5200498434b398284670
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-12-01 21:45:54 +0100

    dd

--- 12_Comunicación-entre-Componentes.jsx
+++ 12_Comunicación-entre-Componentes.jsx
@@ -13,6 +13,6 @@
 
     <h2>Comunicación de un Componente hijo a uno Padre</h2>
 
-    <p> Cuando tenemos la necssidade de passar datos de un Componente Hijo a un Padre lo podemos hacer atraves de los Eventosm, Simplismente passamos el Eventos atraves de props del Componente padre al Componente hijo, para que este Ejecute la funcion</p>
+    <p> Cuando tenemos la necssidade de passar datos de un Componente Hijo a un Padre lo podemos  hacer atraves de los Eventosm, Simplismente passamos el Eventos atraves de props del Componente padre al Componente hijo, para que este Ejecute la funcion</p>
 
 </>
