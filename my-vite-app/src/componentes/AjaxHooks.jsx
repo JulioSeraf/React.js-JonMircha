0b134dbd0676f3b67072be87637e237d20a39dbd
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-12-14 19:09:12 +0100

    Ajax con Hook terminado

--- my-vite-app/src/componentes/AjaxHooks.jsx
+++ my-vite-app/src/componentes/AjaxHooks.jsx
@@ -11,35 +11,62 @@ function Pokemon({avatar, name}){
     )
 }
 
+// export default function PokemonsHooks(){
+//     const [pokeball,setPokeball] = useState([]);
+
+//     useEffect(()=>{
+//         let url = "https://pokeapi.co/api/v2/pokemon/";
+//         fetch(url)
+//         .then(res => res.json())
+//         .then(json => {
+//             json.results.forEach(el => fetch(el.url)
+//             .then(res => res.json())
+//             .then(pokem => {
+//                 let pokemon = {
+//                     id: pokem.id,
+//                     name: pokem.name,
+//                     avatar: pokem.sprites.front_default
+//                 }
+                
+//                setPokeball((pokeball)=> [...pokeball, pokemon])
+//             }))
+//         })
+//     },[])
+
+
+// Ajax con Hooks en usando Funciones Asincronas
+
 export default function PokemonsHooks(){
     const [pokeball,setPokeball] = useState([]);
 
-    let pokemons= [];
-
     useEffect(()=>{
-        let url = "https://pokeapi.co/api/v2/pokemon/";
-        fetch(url)
-        .then(res => res.json())
-        .then(json => {
-            json.results.forEach(el => fetch(el.url)
-            .then(res => res.json())
-            .then(pokem => {
+        // Para usar funciones asincronas en useEffect lo correto es criar na funcion dentro del useEffect para hacerla asincrona ja que tornar la directamente la funcion callback de useEffect asincona el una mala patrica q puede nos causar problemas mas a delante
+
+        const getDataPokemons = async (url)=>{
+            let res = await fetch(url),
+            json = await res.json();
+        
+            json.results.forEach( async (el)=>{
+                let res = await fetch(el.url),
+                pokem = await res.json();
+
                 let pokemon = {
-                    id: pokem.id,
-                    name: pokem.name,
-                    avatar: pokem.sprites.front_default
-                }
-                
-               
-                // setPokeball(pokemons)
-                // console.log(pokeball)
-            }))
-        })
-    })
+                        id: pokem.id,
+                        name: pokem.name,
+                        avatar: pokem.sprites.front_default
+                    }
+                    setPokeball((pokeball)=> [...pokeball, pokemon])
+            })        
+        }
+        getDataPokemons("https://pokeapi.co/api/v2/pokemon/");
+       
+    },[])
+
+
     return(
         <div>
             <h2>Pokemons Con Peticiones Ajax con Hooks</h2>
-            {}
+            {pokeball.length === 0 ? <h3>Cargando..</h3>: pokeball.map((el)=> <Pokemon key={el.id} name={el.name} avatar={el.avatar}/>)}
 
         </div>
     )
