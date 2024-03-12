8a54be6ed27ce682b1b04bc844f68cb37a371a68
julio serafim <juliodev26@gmail.com>
2024-03-12 21:16:40 +0100

    Teotia Referencias

--- my-vite-app/src/componentes/Referencia.jsx
+++ my-vite-app/src/componentes/Referencia.jsx
@@ -13,25 +13,30 @@
 // - A diferencia de estado, asignar el valor de current de una ref no desencadena un rerenderizado.
 
 // - No leas o escribas ref.current durante el renderizado. Esto hace que tu componente sea dificil de predecir.
-import React from "react";
+import React,{createRef,useRef} from "react";
+// usamos el create Ref para crear referencia em componentes de class y el Hooks useRef para criar referencias em componentes fucionales
 
 export default function Referencias(){
+    let refMenu = useRef(),
+        refMenubtn = useRef();
+        // las referencias son obejtos q tiene un propiedad llamada current que es asiguido el elemento que hace referencia, esto es por si el elemento es eliminado current recibe un valor nulo para no salter error 
+        
     const handleToggleMenu = (e)=>{
-        const $menu = document.getElementById("menu");
 
-        if( $menu.style.display === "none"){
-            e.target.textContent = "Cerrar";
-            $menu.style.display = "block";
+        if( refMenu.current.style.display === "none"){
+            refMenubtn.current.textContent = "Cerrar";
+            refMenu.current.style.display = "block";
         }else{
-            $menu.style.display = "none";
-            e.target.textContent = "Menu";
+            refMenu.current.style.display = "none";
+            refMenubtn.current.textContent = "Menu";
         }
-    }
+
+    };
     return(
         <div>
             <h2>Referencias</h2>
-            <button id="menu-btn" onClick={handleToggleMenu}>Menu</button>
-            <nav id="menu" style={{display: "none"}}>
+            <button id="menu-btn" ref={refMenubtn} onClick={handleToggleMenu}>Menu</button>
+            <nav id="menu" ref={refMenu} style={{display: "none"}}>
                 <a href="">section 1</a>
                 <br />
                 <a href="">section 2</a>
