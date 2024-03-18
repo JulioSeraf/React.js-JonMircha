00388254227e6d3c57c41749269799ba3e810d1e
julio serafim <juliodev26@gmail.com>
2024-03-18 16:14:49 +0100

    imput cheked con react

--- my-vite-app/src/componentes/Formularios.jsx
+++ my-vite-app/src/componentes/Formularios.jsx
@@ -3,6 +3,9 @@ import React,{useState} from "react";
 export default function Formulario(e){
     const [nombre, setNombre] = useState("");
     const [sabor, setSabor] = useState("");
+    const [lenguaje, setLenguaje] = useState("");
+    const [terminos, setTerminos] = useState(false);
+
     return( <>
         <h2>Formularios</h2> 
         <form>
@@ -31,6 +34,21 @@ export default function Formulario(e){
              <input type="radio" name="sabor" id="svelte" value="svelte" onChange={(e) => setSabor(e.target.value)} defaultChecked/>
              {/* se queremos un valor renderizado por defecto no usamos checked en React y si defaultChecked */}
              <label htmlFor="svelte">Svelte</label>
+
+             <h3>Elige tu lenguaje de programacion favorito:</h3>
+             <select name="lenguaje" id="" onChange={(e)=> setLenguaje(e.target.value)} defaultValue="">
+                {/* El un select para asignar un valor poe defecto al diferenre de html no usamos el atributo selected en una de las opciones en su lugar usamos el atributo defaultValue directamente en la tag select,como esta arriba*/}
+                <option value="">---</option>
+                <option value="js">JavaScript</option>
+                <option value="php">PHP</option>
+                <option value="py">Python</option>
+                <option value="go">GO</option>
+                <option value="rb">Ruby</option>
+             </select>
+             <br />
+                <label htmlFor="terminos">Acepto los términos y condiciones</label>
+                <input type="checkbox" id="terminos" onChange={(e)=> setTerminos(e.target.checked)}/>
+
         </form>
     </>)
 }
